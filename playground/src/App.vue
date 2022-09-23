57c7818ded2e2e8197f2d22bb5d62ed7dce05f8f
Dylan Zhang <dylanzhang@augmentum.com.cn>
2022-09-23 17:29:31 +0800

    chore: cleanup

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -2,16 +2,16 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
-throw 'Hi'
+throw new Error('In Vue')
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="logo" alt="Vite logo">
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
@@ -23,9 +23,11 @@ throw 'Hi'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import http from 'http'
 import { chromium } from 'playwright'
 import sirv from 'sirv'
-import polka from 'polka'
 import c from 'picocolors'
 import createDebug from 'debug'
 import type { Options, RuntimeLog } from './types'
@@ -10,19 +10,21 @@ const debug = createDebug('deploy-check')
 export async function serveAndCheck(options: Options) {
   const {
     port = 3000,
+    host = '127.0.0.1',
     servePath,
     waitUntil = 'networkidle',
   } = options
 
-  const URL = `http://localhost:${port}`
-
-  polka()
-    .use(sirv(servePath))
-    .listen(3000, (err: any) => {
-      if (err)
-        throw err
-      debug(`> Served on ${URL}`)
-    })
+  const URL = `http://${host}:${port}`
+  const serve = sirv(servePath, {
+    dev: true,
+    single: true,
+    dotfiles: true,
+  })
+  const server = http.createServer((req, res) => {
+    serve(req, res)
+  })
+  server.listen(port, host)
 
   const browser = await chromium.launch()
   debug('> Browser initialed')
@@ -49,22 +51,27 @@ export async function serveAndCheck(options: Options) {
   })
 
   await page.goto(URL, { waitUntil })
-  debug('> Navigated')
+  debug(`> Navigated to ${URL}`)
 
-  Promise.all([
+  await Promise.all([
     page.close(),
-    browser.close(),
-  ]).catch()
+    server.close(),
+  ])
 
   return errorLogs
 }
 
 export function printErrorLogs(logs: RuntimeLog[]) {
+  if (!logs.length) {
+    console.log()
+    console.log(c.inverse(c.bold(c.green(' DEPLOY CHECK '))) + c.green(' No runtime errors found '))
+    return
+  }
   console.error()
   console.error(c.inverse(c.bold(c.red(' DEPLOY CHECK '))) + c.red(` ${logs.length} Runtime errors found`))
   console.error()
   logs.forEach((log, idx) => {
-    console.error(c.yellow(`----------- ${c.gray(new Date(log.timestamp).toLocaleDateString())} Error ${idx + 1} -------------`))
+    console.error(c.yellow(`\n----------- ${c.gray(new Date(log.timestamp).toLocaleDateString())} Error ${idx + 1} -------------`))
     if (log.type === 'error')
       console.error(log.error)
     else
